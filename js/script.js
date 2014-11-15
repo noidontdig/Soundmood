793a1ce29b91c5746c50599f4e677d4a3352b776
Alexandra Qin <alexandraqin@gmail.com>
2014-11-15 09:47:39 -0500

    Fix starter code

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 // ==========================
 
 // List of moods
-var moodList = ['sad', 'afraid', 'abandoned', 'acceptance', 'adoration', 'affection', 'aggravated', 'agitated', 'aggressive', 'alert', 'amazed', 'ambitious', 'amused', 'anger', 'animosity', 'annoyed', 'anticipation', 'anxiousness', 'appreciative', 'apprehensive', 'ardent', 'aroused', 'ashamed', 'astonished', 'attraction', 'awed', 'betrayed', 'bewildered', 'bitter', 'bliss', 'blue', 'boastful', 'bored', 'breathless', 'bubbly', 'calamitous', 'calm', 'camaraderie', 'cautious', 'cheerful', 'cocky', 'cold', 'collected', 'comfortable', 'compassionate', 'concerned', 'confident', 'confused', 'contempt', 'content', 'courageous', 'cowardly', 'crafty', 'cranky', 'crazy', 'cruelty', 'crummy', 'crushed', 'curious', 'cynic', 'dark', 'dejected', 'delighted', 'delirious', 'denial', 'depression', 'desire poem', 'despair', 'determined', 'devastated', 'disappointed', 'discouraged', 'disgust', 'disheartened', 'dismal', 'dispirited', 'distracted', 'distressed', 'dopey', 'down', 'dreadful', 'dreary', 'eager', 'ecstatic', 'embarrassed', 'emotional-detest', 'empathic', 'emptiness', 'enchanted', 'enigmatic', 'enlightened', 'enraged', 'enthralled', 'enthusiastic', 'envy', 'euphoric', 'excited', 'exhausted', 'expectation', 'exuberance', 'fascinated', 'fear', 'flabbergasted', 'fight-or-flight', 'foolish', 'frazzled', 'frustrated', 'fulfillment', 'furious', 'giddy', 'gleeful', 'gloomy', 'goofy', 'grateful', 'gratified', 'greedy', 'grouchy', 'grudging', 'guilty', 'happy', 'hate', 'heartbroken', 'homesick', 'hopeful', 'hopeless', 'horrified', 'hostile', 'humiliated', 'humored', 'hurt', 'hyper', 'hysterical', 'indignation', 'infatuation', 'infuriated', 'inner peace', 'innocent', 'insanity', 'insecure', 'insecure', 'inspired poem', 'interest', 'intimidated', 'invidious', 'irate', 'irritability', 'irritated', 'jaded', 'jealousy', 'joy', 'jubilant', 'kind', 'lazy', 'left out', 'liberated', 'lively', 'loathsome', 'lonely', 'longing', 'love', 'lovesick', 'loyal', 'lust', 'mad', 'mean', 'melancholic', 'mellow', 'mercy', 'merry', 'mildness', 'miserable', 'morbid', 'mourning', 'needed', 'needy', 'nervous', 'obscene', 'obsessed', 'offended', 'optimistic', 'outraged', 'overwhelmed', 'pacified', 'pain', 'panicky', 'paranoia', 'passion', 'pathetic', 'peaceful', 'perturbation', 'pessimistic', 'petrified', 'pity', 'playful', 'pleased', 'pleasure', 'possessive', 'pride', 'provoked', 'proud', 'puzzled', 'rage', 'regretful', 'relief', 'remorse', 'resentment', 'resignation', 'resolved', 'sadness', 'satisfied', 'scared', 'Schadenfreude', 'scorn', 'selfish', 'sensual', 'sensitive', 'sexy', 'shame', 'sheepish', 'shocked', 'shy', 'sincerity', 'solemn', 'somber', 'sorrow', 'sorry', 'spirited', 'stressed', 'strong', 'submissive', 'superior', 'surprised', 'sweet', 'sympathetic', 'temperamental', 'tense', 'terrified', 'threatened', 'thrilled', 'tired', 'tranquil', 'troubled', 'trust', 'tormented', 'uncertainty', 'uneasiness', 'unhappy', 'upset', 'vengeful', 'vicious', 'warm', 'weary', 'worn-out', 'worried', 'worthless', 'wrathful', 'yearning', 'zesty'];
+var moodList = [];
 
 
 // ==============================
@@ -15,8 +15,8 @@ var moodList = ['sad', 'afraid', 'abandoned', 'acceptance', 'adoration', 'affect
 // ==============================
 
 SC.initialize({
-  client_id: '53e1b5c5885286f94a2f2dbe52932379',
-  redirect_uri: 'https://c9.io/jasondecastro/soundcloud/workspace/index.html'
+  client_id: '5aa8e389ba4e24b6106af5159ab3e344',
+  redirect_uri: 'https://soundmoodsolution-noidontdig-1.c9.io/index.html.'
 });
 
 SC.connect(function () {
@@ -28,9 +28,40 @@ SC.connect(function () {
 
 // ===========================
 //
+//        Document ready
+//
+// ===========================
+//
+// # Document ready
+//
+// $(document).ready() runs once the page DOM is ready for JavaScript
+// to execute. A page can't be manipulated safely until the document is ready.
+//
+$(document).ready(function () {
+  // Add click handlers to 'go' and 'random' buttons here.
+});
+
+
+// ===========================
+//
 //        Play a song
 //
 // ===========================
+//
+// # Play a track
+//
+// Play a track using the Souncdloud Javascript SDK
+//
+function playOneTrack () {
+  // TODO: fill this out
+}
+
+
+// ======================================
+//
+//    Play a song for the user's mood
+//
+// ======================================
 
 // The song that is currently playing
 var currentSong;
@@ -39,13 +70,10 @@ var currentSong;
 // # 'Go' button click handler
 //
 // Get the user's mood and play a song that matches that mood.
-// (Optional: change the color of the jumbotron)
 //
-$('#go').on('click', function () {
-  var mood = $('#mood').val();
-  updateJumboTron(mood);
-  searchTracks(mood);
-});
+function goClicked () {
+  // TODO: fill this out
+}
 
 //
 // # Search soundcloud tracks
@@ -56,13 +84,7 @@ $('#go').on('click', function () {
 // * **mood**, the user's mood.
 //
 function searchTracks (mood) {
-  SC.get('/tracks', { q: mood }, function (tracks) {
-    var songs = tracks;
-    var randomSongNumber = Math.floor(Math.random() * (songs.length - 1));
-    var song = songs[randomSongNumber];
-    playTrack(song.id);
-    $('#songtitle').html(song.user.username + '' + song.title);
-  });
+  // TODO: fill this out
 }
 
 //
@@ -71,29 +93,23 @@ function searchTracks (mood) {
 // Play a Soundcloud track. If there is already a song playing,
 // stop that song first.
 //
+// Use 'currentSong' to keep track of the song that is playing.
+//
 // * **trackid**, the ID of the track to play.
 //
 function playTrack (trackid) {
-  if(currentSong){
-    currentSong.stop();
-  }
-  SC.stream('/tracks/' + trackid, function (sound) {
-    currentSong = sound;
-    currentSong.play();
-  });
+  // TODO: fill this out
 }
 
 //
 // # Update Jumbotron
 //
 // Updates the header text to show the user's mood.
-// (Optional: change the jumbotron's color)
 //
 // * **mood**, the user's mood
 //
 function updateJumboTron (mood) {
   $('#moodstatus').html('It sounds like you are in a ' + mood +  ' mood!!');
-  changeColor(getColor(mood));
 }
 
 
@@ -108,20 +124,17 @@ function updateJumboTron (mood) {
 //
 // Pick a mood at random from moodList and find a track for that mood.
 //
-$('#random').on('click', function () {
-  var mood = randomMood();
-  updateJumboTron(mood);
-  searchTracks(mood);
-});
+function randomClicked () {
+  // TODO: fill this out
+}
 
 //
 // # Random Mood
 //
-// Returns a random mood from moodList
+// Returns a random mood from moodList.
 //
 function randomMood () {
-  var randomNumber = Math.floor(Math.random() * (moodList.length - 1));
-  return moodList[randomNumber];
+  // TODO: fill this out
 }
 
 
@@ -140,8 +153,6 @@ function randomMood () {
 
 //
 // 1. Change color
-//
-// Add to updateJumbotron: changeColor(getColor(mood));
 //
 
 // Map a few of our moods to 'happy' or 'sad'
@@ -165,10 +176,12 @@ var colorMap = {
 //
 // # Change the color of the jumbotron
 //
+// Update the background-color of the jumbotron using jQuery
+//
 // * **color**, the color to change to
 //
 function changeColor (color) {
-  $('.jumbotron').css('background-color', color);
+  // TODO: fill this out
 }
 
 //
@@ -180,15 +193,7 @@ function changeColor (color) {
 // * **mood**, the user's mood
 //
 function getColor (mood) {
-  var color = '';
-  if (moodMap[mood]) {
-    var feeling = moodMap[mood];
-    var listLength = colorMap[feeling].length;
-    var randomNumber = Math.floor(Math.random() * (listLength - 1));
-    return '#' + colorMap[feeling][randomNumber];
-  } else {
-    return randomColor();
-  }
+  // TODO: fill this out
 }
 
 //
@@ -208,15 +213,5 @@ function randomColor () {
 //
 // 2. Typeahead
 //
-
-//
-// # Document ready
-//
-// $(document).ready() runs once the page DOM is ready for JavaScript
-// to execute. A page can't be manipulated safely until the document is ready.
-//
 // Add a typeahead to the mood input field using the moodList as a source.
 //
-$(document).ready(function () {
-  $('#mood').typeahead({ source: moodList });
-});
